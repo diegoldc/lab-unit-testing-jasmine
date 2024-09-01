3de755df92e38dc3a1450a5d9589510f333e9dae
Diego Lázaro <dlazarodecastro@gmail.com>
2024-09-01 12:05:50 +0200

    lab terminado jasmine

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,27 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 3", () => {
     describe("Function - calculateArea", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(add).toBeDefined();
+        });
+
+        it("should take two numbers as arguments", () => {
+            expect(add.length).toBe(2);
+        });
+
+        it("should return the product of the two numbers", () => {
+            expect(add(10, 2)).toEqual(20);
+            expect(add(5, 1)).toEqual(5);
+            expect(add(100, 50)).toEqual(5000);
+            expect(add(4, 2)).toEqual(8);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(add(1)).toEqual(undefined);
+            expect(add()).toEqual(undefined);
+            expect(add(undefined, 1)).toEqual(undefined);
+        });
 
     })    
 })
-
-*/
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,26 @@
-// UNCOMMENT THE CODE BELOW TO START
+describe("Iteration 2 | Divide", () => {
+    describe("Function divide", () => {
+        it("should be defined", () => {
+            expect(add).toBeDefined();
+        });
 
-/*
+        it("should take two numbers as arguments", () => {
+            expect(add.length).toBe(2);
+        });
 
-describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should return the division of the two numbers", () => {
+            expect(add(10, 2)).toEqual(5);
+            expect(add(5, 1)).toEqual(5);
+            expect(add(100, 50)).toEqual(2);
+            expect(add(4, 2)).toEqual(2);
+        });
 
-    })    
-})
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(add(1)).toEqual(undefined);
+            expect(add()).toEqual(undefined);
+            expect(add(undefined, 1)).toEqual(undefined);
+        });
 
-*/
+
+    });
+});
